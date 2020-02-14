4238e1c2edc01e0fd4f54aea480c436dd602545a
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2020-02-14 07:51:55 +0600

    [implementation] merge sort (n*log(n))

--- Sort.py
+++ Sort.py
@@ -13,10 +13,6 @@ def heap_sort(arr):
     pass
 
 
-def merge_sort(arr):
-    pass
-
-
 def bubble_sort(arr):
     if len(arr) < 2:
         return arr
@@ -41,6 +37,49 @@ def insertion_sort(arr):
     return narr
 
 
+def merge_sort(arr, low=0, hi=None):
+    if hi is None:
+        hi = len(arr) - 1
+    if hi == low:
+        return arr
+
+    mid = (hi + low) // 2
+
+    merge_sort(arr, low, mid)
+    merge_sort(arr, mid + 1, hi)
+
+    l1 = arr[low:mid + 1]
+    l2 = arr[mid + 1:hi + 1]
+
+    i1 = len(l1) - 1
+    i2 = len(l2) - 1
+
+    while i1 > -1 and i2 > -1:
+        if l1[i1] > l2[i2]:
+            arr[hi] = l1[i1]
+            i1 -= 1
+        else:
+            arr[hi] = l2[i2]
+            i2 -= 1
+        hi -= 1
+
+    while i1 > -1:
+        arr[low + i1] = l1[i1]
+        i1 -= 1
+
+    while i2 > -1:
+        arr[low + i2] = l2[i2]
+        i2 -= 1
+
+    return arr
+
+
 if __name__ == '__main__':
-    print(insertion_sort(get_random_numbers(10 ** 3)))
-    print(bubble_sort(get_random_numbers(10 ** 3)))
+    # n^2 sorting algorithms
+    numbers = get_random_numbers(10 ** 3)
+    print(insertion_sort(numbers.copy()))
+    print(bubble_sort(numbers.copy()))
+
+    # n*log(n) sorting algorithms
+    numbers = get_random_numbers(10 ** 5)
+    print(merge_sort(numbers.copy()))
